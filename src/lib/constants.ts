f24da3bef1ca74d37a311fb63bafd588f5ecb29c
nikshaan <shettynikshaan@gmail.com>
2025-09-15 14:38:53 +0530

    add: setup & navbar

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const LINK = {
+    github: "https://github.com/Nikshaan",
+    linkedlin: "https://www.linkedin.com/in/nikshaan-shetty/",
+    email: "mailto:nikshaanshetty2006@gmail.com",
+}
